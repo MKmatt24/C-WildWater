2b0ad43107117aacbda8f9ab6e9087db9c1c08be
M4x0u77 <max19.monteil@gmail.com>
2025-12-08 16:33:41 +0100

    Add files via upload

---
+++ lecode/src/recherche_ligne.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+#define MAX_LINE 1024
+
+
+char *find_line(const char *filename, const char *target)
+{
+    FILE *f = fopen(filename, "r");
+    if (!f) {
+        printf("Error file do not open");
+        exit(2);
+    }
+
+    char buffer[MAX_LINE];
+
+    // fscanf lit jusqu'à la fin de ligne (le \n est consommé grâce à \n final)
+    while (fscanf(f, "%1023[^\n]\n", buffer) == 1) {
+
+        // Vérifie si la ligne contient la clé recherchée
+        if (strstr(buffer, target) != NULL) {
+
+            // Copie dynamique de la ligne trouvée
+            char *result = malloc(strlen(buffer) + 1);
+            if (result)
+                strcpy(result, buffer);
+
+            fclose(f);
+            return result;
+        }
+    }
+
+    fclose(f);
+    return NULL;  // Rien trouvé
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
