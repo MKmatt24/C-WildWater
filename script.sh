f5d878eb8b5df7bc1af4350b4859705616f54174
crayfr <frej.rayene@gmail.com>
2025-12-09 11:24:54 +0100

    ajout de la fonction de la durée de temps

--- script.sh
+++ script.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-start=`date +%s`
+START_TIME=$(date +%s%3N)
 
+
+#affiche la durée totale en millisecondes
+function display_time {
+  if [ ! -z "$START_TIME" ]; then
+    END_TIME=$(date +%s%3N)
+    DURATION=$((END_TIME - START_TIME))
+    echo "Temps d'éxecution total: ${DURATION} ms"
+  fi
+}
+
+
+
+#affiche le temps d'éxecution en fin de script ou en cas d'erreur
+trap display_time EXIT
 cat << "EOF"
    ___  __      ___ _    ___      __    _           
   / __|_\ \    / (_) |__| \ \    / /_ _| |_ ___ _ _ 
@@ -15,4 +29,9 @@ echo Bienvenue sur C-WildWater!
 end=`date +%s`
 
 runtime=$((end-start))
+
+echo "Press enter to quit..."
+read dummy
+
+echo $$ $BASHPID - | cat -
 echo "Temps d'execution: $runtime seconds"
