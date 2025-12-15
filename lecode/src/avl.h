a568409bc0317142c374049b05e5ec69f7cb71c4
MKmatt24 <mkmatt2405@gmail.com>
2025-12-15 14:55:41 +0100

    Modifs

--- lecode/src/avl.h
+++ lecode/src/avl.h
@@ -24,7 +24,7 @@ typedef struct AVLNode {
     int height;
 } AVLNode;
 
-// Prototypes des fonctions
+// Prototypes of functions
 AVLNode* avl_create_node(void *data);
 AVLNode* avl_insert(AVLNode *root, void *data, int (*compare)(const void*, const void*));
 void* avl_search(AVLNode *root, const char *id, int (*compare)(const void*, const char*));
@@ -32,13 +32,13 @@ void avl_inorder(AVLNode *root, FILE *file, void (*print_data)(void*, FILE*));
 void avl_inorder_reverse(AVLNode *root, FILE *file, void (*print_data)(void*, FILE*));
 void avl_free(AVLNode *root, void (*free_data)(void*));
 
-// Fonctions de comparaison
+// Comparaison functions
 int compare_factories(const void *a, const void *b);
 int search_factory(const void *a, const char *id);
 int compare_network_nodes(const void *a, const void *b);
 int search_network_node(const void *a, const char *id);
 
-// Fonctions d'affichage et de libération
+// Dipslay and release functions
 void print_factory(void *data, FILE *file);
 void free_factory(void *data);
 void print_network_node(void *data, FILE *file);
