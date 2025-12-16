954e0e22da6f8b7f75e7132e5eb3f84d7afa51fb
M4x0u77 <max19.monteil@gmail.com>
2025-12-16 12:11:43 +0100

    Add files via upload

---
+++ lecode/src/main.c
@@ -0,0 +1,36 @@
+#include"avl.h"
+#include"function.h"
+
+
+
+
+
+
+int main() {
+
+	
+	build_avl(); //build avl from file
+	
+	
+	if(max) {
+	
+		max_capacity();
+	}
+	
+	if(src) {
+	
+		captation_vol();
+	}
+	
+	if(real) {
+	
+		real_vol();
+	}
+	
+	
+return 0;
+}
+
+
+
+
