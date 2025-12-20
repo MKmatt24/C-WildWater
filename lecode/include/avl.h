41d94ed396ea5a28199bee6dc4a4aab84754318f
M4x0u77 <max19.monteil@gmail.com>
2025-12-20 11:26:20 +0100

    Update avl.h

--- lecode/include/avl.h
+++ lecode/include/avl.h
@@ -1,22 +1,10 @@
+
 #ifndef AVL_H
 #define AVL_H
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
-typedef struct Factory {
-    char *id;
-    double max_volume;
-    double source_volume;
-    double real_volume;
-} Factory;
-
-typedef struct NetworkNode {
-    char *id;
-    struct NetworkNode *parent;
-} NetworkNode;
-
+// Structure de l'arbre AVL générique
 typedef struct AVLNode {
     void *data;
     struct AVLNode *left;
@@ -24,24 +12,16 @@ typedef struct AVLNode {
     int height;
 } AVLNode;
 
-// Prototypes of functions
+// Fonctions de base de l'AVL
 AVLNode* avl_create_node(void *data);
+int avl_height(AVLNode *node);
+int avl_max(int a, int b);
+AVLNode* avl_rotate_right(AVLNode *y);
+AVLNode* avl_rotate_left(AVLNode *x);
+int avl_get_balance(AVLNode *node);
 AVLNode* avl_insert(AVLNode *root, void *data, int (*compare)(const void*, const void*));
 void* avl_search(AVLNode *root, const char *id, int (*compare)(const void*, const char*));
-void avl_inorder(AVLNode *root, FILE *file, void (*print_data)(void*, FILE*));
 void avl_inorder_reverse(AVLNode *root, FILE *file, void (*print_data)(void*, FILE*));
 void avl_free(AVLNode *root, void (*free_data)(void*));
-
-// Comparaison functions
-int compare_factories(const void *a, const void *b);
-int search_factory(const void *a, const char *id);
-int compare_network_nodes(const void *a, const void *b);
-int search_network_node(const void *a, const char *id);
-
-// Dipslay and release functions
-void print_factory(void *data, FILE *file);
-void free_factory(void *data);
-void print_network_node(void *data, FILE *file);
-void free_network_node(void *data);
 
 #endif
